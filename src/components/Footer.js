af6c609a1d95100b37f98e31f4d585505cc8612c
chaitanyashimpi <chaitanyashimpi2001@gmail.com>
2023-05-23 22:31:32 +0530

    Project

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Footer = () => (
+  <Box mt="80px" p="5px" bgcolor="#FFF3F4">
+
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by Cratonik</Typography>
+  </Box>
+);
+
+export default Footer;
